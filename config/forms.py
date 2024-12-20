aae1198c11b195fb223588d7140b1bce8ded13e1
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-12-20 11:01:55 +0700

    init

---
+++ config/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate
+
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+class CustomAuthenticationForm(AuthenticationForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.fields['username'].widget.attrs.update({
+            'class': 'input-primary',
+        })
+        self.fields['username'].label = 'Your username'
+        self.fields['password'].label = 'Your password'
+        self.fields['username'].label_suffix = False
+        self.fields['password'].widget.attrs.update({
+            'class': 'input-primary',
+        })
---
+++ config/views.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.views import LoginView
+from .forms import CustomAuthenticationForm
+
+class CustomLoginView(LoginView):
+
+    authentication_form = CustomAuthenticationForm
+    redirect_authenticated_user = False
+    template_name = "registration/login.html"
