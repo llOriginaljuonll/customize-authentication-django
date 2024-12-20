a495aa9e2161a760945905dc94540b23d086ce54
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-12-21 02:30:24 +0700

    create logout button and backend

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 app_name = "accounts"
 
 urlpatterns = [
     path("login/", views.CustomLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
     path("signup/", views.CustomUserCreationView.as_view(), name="signup"),
 ]
